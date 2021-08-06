341b3714d93b03bafb6a0ea3047f5de5101b7e33
aronyu <h0935674938@hotmail.com>
2021-08-06 23:29:49 +0800

    init gem

---
+++ spec/ftx/ruby_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ftx::Api do
+  it "has a version number" do
+    expect(Ftx::Api::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
